b750ac7846912b0d58e990aced95a462170be124
MUKIL KUMAR <mukil289446@gmail.com>
2023-04-07 10:58:21 +0530

    lts
    good

--- pages/works.js
+++ pages/works.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import {Stack,Image,Text,Button,useDisclosure,Collapse,Box } from '@chakra-ui/react'
@@ -248,194 +248,194 @@ One of my projects was to visualize the 3D Drone concept under the mentorship of
 // ================ End of Work Section =========================
 
 // ============= Coll ===========================================
-function CollapseExCol() {
-  const { isOpen, onToggle } = useDisclosure()
-  return (
-    <>
-      <Button onClick={onToggle} variant="ghost" fontSize={20}>Collaborations</Button>
-      <Collapse in={isOpen} animateOpacity>
-        <Box
-          p='1px'
-          color='white'
-          mt='4'
-          rounded='md'
-          shadow='md'
-        >
-         {/* Works Content */}
-         <SimpleGrid columns={[1, 2, 3]} gap={10} mt={10}>
-        {/* Starting of Cards - Add or Remove Work cards here */}
-          {/* Bugatti + Mclaren */}
-            <Section>
-        {/* Bugatti 1 Car Content 1 */}
-        <Container mb={10}>
-            <Image 
-          src='/images/works/Bugatti1.jpg'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            Car is cathe text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-          <Container>
-            <Image 
-          src='/images/works/mclar.jpg'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            LOREM IPSUM
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-            </Section>    
-          {/* Bugatti + Mclaren */}
-      <Section>
-      <Container>
-            <Image 
-          src='/images/works/drone1.png'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            LOREM IPSUM
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-      </Section>
+// function CollapseExCol() {
+//   const { isOpen, onToggle } = useDisclosure()
+//   return (
+//     <>
+//       <Button onClick={onToggle} variant="ghost" fontSize={20}>Collaborations</Button>
+//       <Collapse in={isOpen} animateOpacity>
+//         <Box
+//           p='1px'
+//           color='white'
+//           mt='4'
+//           rounded='md'
+//           shadow='md'
+//         >
+//          {/* Works Content */}
+//          <SimpleGrid columns={[1, 2, 3]} gap={10} mt={10}>
+//         {/* Starting of Cards - Add or Remove Work cards here */}
+//           {/* Bugatti + Mclaren */}
+//             <Section>
+//         {/* Bugatti 1 Car Content 1 */}
+//         <Container mb={10}>
+//             <Image 
+//           src='/images/works/Bugatti1.jpg'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             Car is cathe text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//           <Container>
+//             <Image 
+//           src='/images/works/mclar.jpg'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             LOREM IPSUM
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//             </Section>    
+//           {/* Bugatti + Mclaren */}
+//       <Section>
+//       <Container>
+//             <Image 
+//           src='/images/works/drone1.png'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             LOREM IPSUM
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//       </Section>
 
-      <Section>
-      <Container>
-            <Image 
-          src='/images/works/drone2.png'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-      </Section>
+//       <Section>
+//       <Container>
+//             <Image 
+//           src='/images/works/drone2.png'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//       </Section>
 
-      <Section>
-      <Container>
-            <Image 
-          src='/images/works/drone3.png'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-      </Section>
+//       <Section>
+//       <Container>
+//             <Image 
+//           src='/images/works/drone3.png'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//       </Section>
 
-      <Section>
-      <Container>
-            <Image 
-          src='/images/works/lambo1.jpg'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-      </Section>
+//       <Section>
+//       <Container>
+//             <Image 
+//           src='/images/works/lambo1.jpg'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//       </Section>
 
-      <Section>
-      <Container>
-            <Image 
-          src='/images/works/drone4.png'
-          alt='Cahar'
-          borderRadius='lg'
-          />
-          <Stack mt='6' spacing='3'>
-          <Heading as="h2"  fontWeight="hairline">
-          BUGATTI CHIRON
-        </Heading>
-          <Text fontSize={'xs'}>
-            Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
-            Do Project Description your Experience or whatsoever you want!
-          </Text>
-          <Text color='blue.600' fontSize='2xl'>
-            Temp link
-          </Text>
-        </Stack>
-          </Container>
-      </Section>
-      {/* Ending of Cards - Add or Remove Work cards here */}
-      </SimpleGrid>
-         {/* Works Content */}
-        </Box>
-      </Collapse>
-    </>
-  )
-}
+//       <Section>
+//       <Container>
+//             <Image 
+//           src='/images/works/drone4.png'
+//           alt='Cahar'
+//           borderRadius='lg'
+//           />
+//           <Stack mt='6' spacing='3'>
+//           <Heading as="h2"  fontWeight="hairline">
+//           BUGATTI CHIRON
+//         </Heading>
+//           <Text fontSize={'xs'}>
+//             Car is car the text is just template to increase lines here thake this The quick brown fox jumps over the lazy dog
+//             Do Project Description your Experience or whatsoever you want!
+//           </Text>
+//           <Text color='blue.600' fontSize='2xl'>
+//             Temp link
+//           </Text>
+//         </Stack>
+//           </Container>
+//       </Section>
+//       {/* Ending of Cards - Add or Remove Work cards here */}
+//       </SimpleGrid>
+//          {/* Works Content */}
+//         </Box>
+//       </Collapse>
+//     </>
+//   )
+// }
 
 const Works = () => (
   
