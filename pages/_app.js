20e9343fbb4a28f712ac41490e563982af3a1952
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-05-14 20:47:59 +0530

    les go
    les go

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-
-
-
 import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
+import '../styles/cursor.css'
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
